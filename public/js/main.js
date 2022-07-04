31e20784c0acdcc51479b533dfadc618dc47d0b6
youngbin <redmangos@naver.com>
2022-07-04 22:23:07 +0900

    add Sphere

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+import * as THREE from "/three/build/three.module.js";
+
+let scene, camera, renderer, canvas;
+
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+let geometry = new THREE.SphereGeometry(1, 32, 32);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let earthmesh = new THREE.Mesh(geometry, material);
+scene.add(earthmesh);
+
+let light = new THREE.DirectionalLight(0xffffff);
+let helper = new THREE.DirectionalLightHelper(light, 5);
+scene.add(helper);
+
+camera.position.z = 5;
+
+canvas = document.querySelector(".c");
+renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", onWindowResize, false);
+
+function resizeRendererToDisplaySize(renderer) {
+  canvas = renderer.domElement;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+  const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateWorldMatrix();
+  }
+}
+
+animate();
---
+++ server.js
@@ -0,0 +1,20 @@
+let PORT = 3000;
+
+// node_modules 에 있는 express 관련 파일을 가져온다.
+let express = require("express");
+
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+let app = express();
+// 최상위 폴더 사용 허용
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
+
+// 맨 처음 화면을 index.html로 설정
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// 3000 포트로 서버 오픈
+app.listen(PORT, function () {
+  console.log(`START! Express server on port ${PORT}`);
+});
