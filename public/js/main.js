745d434922655a15d1e18e387fadf5533a1fa0d5
youngbin <redmangos@naver.com>
2022-07-05 22:33:04 +0900

    earth texturemap

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "/three/build/three.module.js";
-
+import { TextureLoader } from "/three/src/loaders/TextureLoader.js";
 let scene, camera, renderer, canvas;
 
 scene = new THREE.Scene();
@@ -10,16 +10,20 @@ camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const texture = new THREE.TextureLoader().load("/images/earth_texture.jpg");
+
 let geometry = new THREE.SphereGeometry(1, 32, 32);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let material = new THREE.MeshBasicMaterial({ map: texture });
 let earthmesh = new THREE.Mesh(geometry, material);
 scene.add(earthmesh);
+
+scene.add(new THREE.AmbientLight(0x333333));
 
 let light = new THREE.DirectionalLight(0xffffff);
 let helper = new THREE.DirectionalLightHelper(light, 5);
 scene.add(helper);
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 canvas = document.querySelector(".c");
 renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
