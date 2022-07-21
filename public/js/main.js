2456a9e7d6ed77a95d863b7e96bdc3ffd6fb6382
youngbin <redmangos@naver.com>
2022-07-21 22:35:54 +0900

    texture

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "/three/build/three.module.js";
 
 let scene, camera, renderer, canvas, raycaster, pointer, section;
+let ambient, light, loader, urls;
 
 window.addEventListener("scroll", function () {
   let header = this.document.querySelector("header");
@@ -16,17 +17,40 @@ camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+ambient = new THREE.AmbientLight(0x222222);
+scene.add(ambient);
+
+light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 0, 6);
+scene.add(light);
+
+loader = new THREE.TextureLoader();
+
+urls = [
+  "edge.png",
+  "spine.png",
+  "top.png",
+  "bottom.png",
+  "front.png",
+  "back.png",
+];
+
+const materials = urls.map((url) => {
+  return new THREE.MeshLambertMaterial({
+    map: loader.load(),
+  });
+});
 
 canvas = document.getElementById("c");
 renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 section.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(3.5, 5, 0.5);
+
+const cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 6;
 
 raycaster = new THREE.Raycaster();
 pointer = new THREE.Vector2();
@@ -55,14 +79,14 @@ function resizeRendererToDisplaySize(renderer) {
 }
 
 function animate() {
-  raycaster.setFromCamera(pointer, camera);
+  //raycaster.setFromCamera(pointer, camera);
 
   // calculate objects intersecting the picking ray
-  const intersects = raycaster.intersectObjects(scene.children);
+  //const intersects = raycaster.intersectObjects(scene.children);
 
-  for (let i = 0; i < intersects.length; i++) {
-    intersects[i].object.material.color.set(0xff0000);
-  }
+  //for (let i = 0; i < intersects.length; i++) {
+  //  intersects[i].object.material.color.set(0xff0000);
+  //}
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
